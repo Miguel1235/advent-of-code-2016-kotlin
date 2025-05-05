652c6417118f172e9b2034b20dc15c90669d7ae1
Miguel Del Corso <migueldelcorso@gmail.com>
2025-05-05 11:56:16 -0300

    Finished part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -9,15 +9,6 @@ private fun isValidTriangle(originalList: List<Int>, combinations: List<List<Int
     } == 3
 }
 
-private fun part1(input: List<String>): Int {
-    val triangleRegex = Regex("""(\d+)\s+(\d+)\s+(\d+)""")
-    return input.sumOf {
-        val result = triangleRegex.find(it)!!.groupValues.drop(1).map { it.toInt() }
-        val combinations = combinations(result, 2)
-        if(isValidTriangle(result, combinations)) 1 else 0.toInt()
-    }
-}
-
 
 fun findValuesNotInCombination(originalList: List<Int>, combination: List<Int>): List<Int> {
     val originalFrequency = originalList.groupingBy { it }.eachCount()
@@ -36,17 +27,32 @@ fun findValuesNotInCombination(originalList: List<Int>, combination: List<Int>):
     return result
 }
 
-private fun part2(input: List<String>): Int {
-    return 0
+private fun parseInput(input: List<String>): List<List<Int>> {
+    val triangleRegex = Regex("""(\d+)\s+(\d+)\s+(\d+)""")
+    return input.map { it ->
+        triangleRegex.find(it)!!.groupValues.drop(1).map { it.toInt() }
+    }
+}
+
+private fun part1(input: List<List<Int>>): Int {
+    return input.sumOf {
+        val combinations = combinations(it, 2)
+        if(isValidTriangle(it, combinations)) 1 else 0.toInt()
+    }
+}
+
+private fun part2(input: List<List<Int>>): Int {
+    val newInput = (input.map { it[0] } + input.map { it[1] } + input.map { it[2] }).chunked(3)
+    return part1(newInput)
 }
 
 fun main() {
-    val testInput = readInput("Day03_test")
-    check(part1(testInput) == 0)
-//    check(part2(testInput) == 0)
+    val testInput = parseInput(readInput("Day03_test"))
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    val input = readInput("Day03")
+    val input = parseInput(readInput("Day03"))
     check(part1(input) == 869)
-//    check(part2(input) == 0)
+    check(part2(input) == 1544)
 }
  
