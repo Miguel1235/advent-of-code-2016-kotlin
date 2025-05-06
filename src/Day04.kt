cc33ab32ffba0c5b07d0b5677e7d1f7618ca9c5e
Miguel Del Corso <migueldelcorso@gmail.com>
2025-05-06 11:01:08 -0300

    Finished Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,3 +1,14 @@
+private fun offsetLetter(letter: Char, offset: Int): Char {
+    val lower = letter.lowercaseChar()
+    if(lower == '-') return ' '
+
+    val baseP = lower - 'a'
+    val newP = (baseP + offset) % 26
+
+    val wrappedPosition = if (newP < 0) newP + 26 else newP
+    return 'a' + wrappedPosition
+}
+
 private fun part1(input: List<String>): Int {
     val regex = Regex("""^((?:[a-zA-Z]+-)+[a-zA-Z]+)-(\d+)\[([a-zA-Z]+)]$""")
     return input.sumOf { line ->
@@ -11,16 +22,20 @@ private fun part1(input: List<String>): Int {
 }
 
 private fun part2(input: List<String>): Int {
-    return 0
+    val regex = Regex("""^((?:[a-zA-Z]+-)+[a-zA-Z]+)-(\d+)\[([a-zA-Z]+)]$""")
+    return input.find { line ->
+        val (letters, sectorId, checksum) = regex.find(line)!!.groupValues.drop(1)
+        val result = letters.map { offsetLetter(it, sectorId.toInt()) }.joinToString("")
+        result == "northpole object storage"
+    }!!.filter { it.isDigit() }.toInt()
 }
 
 fun main() {
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 1514)
-    check(part2(testInput) == 0)
-     
+
     val input = readInput("Day04")
     check(part1(input) == 278221)
-//    check(part2(input) == 0)
+    check(part2(input) == 267)
 }
  
