b6df76d165e4c97ebbfca32fd9a0eb9e29685588
Miguel Del Corso <migueldelcorso@gmail.com>
2025-05-06 10:33:15 -0300

    Finished part 1 of day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,5 +1,13 @@
 private fun part1(input: List<String>): Int {
-    return 0
+    val regex = Regex("""^((?:[a-zA-Z]+-)+[a-zA-Z]+)-(\d+)\[([a-zA-Z]+)]$""")
+    return input.sumOf { line ->
+            val (letters, sectorId, checksum) = regex.find(line)!!.groupValues.drop(1)
+            val joinedRooms = letters.split("-").joinToString("") { it.trim() }
+            val sortedPairs = joinedRooms.groupingBy { it }.eachCount().toList().sortedWith(compareByDescending<Pair<Char, Int>> { it.second }.thenBy { it.first })
+            val sortedList = sortedPairs.take(5).map { it.first }.joinToString("")
+
+            if(sortedList == checksum) sectorId.toInt() else 0
+    }
 }
 
 private fun part2(input: List<String>): Int {
@@ -8,11 +16,11 @@ private fun part2(input: List<String>): Int {
 
 fun main() {
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 1514)
     check(part2(testInput) == 0)
      
     val input = readInput("Day04")
-    check(part1(input) == 0)
-    check(part2(input) == 0)
+    check(part1(input) == 278221)
+//    check(part2(input) == 0)
 }
  
