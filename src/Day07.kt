5687adbf86cf0a20ebb0d0c73a5d11d239ace538
Miguel Del Corso <migueldelcorso@gmail.com>
2025-05-09 23:37:13 -0300

    Finished Day07

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,45 +1,71 @@
-private fun part1(input: List<String>): Int {
+private fun supportsTls(toCheck: List<String>): Boolean {
+    for (line in toCheck) {
+        val f = line.chunked(2)
+        val s = line.drop(1).chunked(2)
+        val r1 = f.zipWithNext().any { (f, s) -> (f == s.reversed() || f.reversed() == s) && f != s }
+        val r2 = s.zipWithNext().any { (f, s) -> (f == s.reversed() || f.reversed() == s) && f != s }
+
+        if (r1 || r2) return true
+    }
+    return false
+}
+
+private fun parseInput(input: List<String>): List<List<String>> {
     val regex = Regex("""\w+""")
 
-    fun supportsTls(toCheck: List<String>): Boolean {
-        for(line in toCheck) {
-            val f = line.chunked(2)
-            val s = line.drop(1).chunked(2)
-            val r1 = f.zipWithNext().any { (f,s) -> (f == s.reversed() || f.reversed() == s) && f != s }
-            val r2 = s.zipWithNext().any { (f,s) -> (f == s.reversed() || f.reversed() == s) && f != s }
-
-            if(r1 || r2) return true
+    return buildList {
+        for (line in input) {
+            add(regex.findAll(line).toList().map { it.value })
         }
-        return false
     }
+}
 
+private fun part1(input: List<List<String>>): Int {
     var total = 0
+    for (line in input) {
+        val brackets = line.filterIndexed { index, _ -> index % 2 == 1 }
+        val notBracket = line.filterIndexed { index, _ -> index % 2 == 0 }
 
-    for(line in input) {
-        val r = regex.findAll(line).toList().map { it.value }
-
-        val brackets = r.filterIndexed { index, _ -> index % 2 == 1 }
-        val notBracket = r.filterIndexed { index, _ -> index % 2 == 0 }
-
-        if(supportsTls(notBracket) && !supportsTls(brackets)) {
+        if (supportsTls(notBracket) && !supportsTls(brackets)) {
             total++
         }
     }
-
     return total
 }
 
-private fun part2(input: List<String>): Int {
-    return 0
+private fun part2(input: List<List<String>>): Int {
+    var total = 0
+
+    for (line in input) {
+        val brackets = line.filterIndexed { index, _ -> index % 2 == 1 }
+        val notBracket = line.filterIndexed { index, _ -> index % 2 == 0 }
+
+        for (bracket in brackets) {
+
+            val all = bracket.windowed(3)
+
+            for (third in all) {
+                val (s, m, e) = third.toList()
+                if (s == e) {
+                    val op = "$m$e$m"
+                    if (notBracket.any { it.contains(op) }) {
+                        total++
+                        break
+                    }
+                }
+            }
+        }
+    }
+    return total
 }
 
 fun main() {
-    val testInput = readInput("Day07_test")
-    check(part1(testInput) == 2)
-//    check(part2(testInput) == 0)
-     
-    val input = readInput("Day07")
+    val testInput = parseInput(readInput("Day07_test"))
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 3)
+
+    val input = parseInput(readInput("Day07"))
     check(part1(input) == 118)
-//    check(part2(input) == 0)
+    check(part2(input) == 260)
 }
  
