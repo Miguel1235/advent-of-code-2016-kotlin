f64837d68dc6285011a678c90f51a7052f61059e
Miguel Del Corso <migueldelcorso@gmail.com>
2025-05-22 09:49:15 -0300

    Made some improvements on the day16

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,32 +1,30 @@
-private fun part(data: String, diskLength: Int = 20): String {
+private val part = { data: String, diskLength: Int ->
     var dragonCurve = obtainDragonCurve(data)
-    while(dragonCurve.length < diskLength) {
+    while (dragonCurve.length < diskLength) {
         dragonCurve = obtainDragonCurve(dragonCurve)
     }
-    dragonCurve = dragonCurve.substring(0, diskLength)
-    val checksum = obtainChecksum(dragonCurve)
-    return checksum
+    obtainChecksum(dragonCurve.take(diskLength))
 }
 
-private fun obtainChecksum(dragonCurve: String): String {
-    val pairs = dragonCurve.windowed(2,2, false)
-    var resultString = pairs.map { if(it[0] == it[1]) '1' else '0' }.joinToString("")
-    while(resultString.length % 2 == 0) {
-        val pairs = resultString.windowed(2,2, false)
-        resultString = pairs.map { if(it[0] == it[1]) '1' else '0' }.joinToString("")
+private val obtainChecksum = { dragonCurve: String ->
+    var result = dragonCurve
+    do {
+        result = result.chunked(2).map { if (it[0] == it[1]) '1' else '0' }.joinToString("")
+    } while (result.length % 2 == 0)
+    result
+}
+
+private val obtainDragonCurve = { data: String ->
+    buildString {
+        append(data)
+        append('0')
+        append(data.reversed().map { if (it == '0') '1' else '0' }.joinToString(""))
     }
-    return resultString
-}
-
-private fun obtainDragonCurve(data: String): String {
-    val a = data
-    val b = a.reversed().map { if(it == '0') '1' else '0' }.joinToString("")
-    return "${a}0${b}"
 }
 
 fun main() {
     val testInput = readInput("Day16_test").first()
-    check(part(testInput) == "01100")
+    check(part(testInput, 20) == "01100")
 
     val input = readInput("Day16").first()
     check(part(input, 272) == "11100110111101110")
