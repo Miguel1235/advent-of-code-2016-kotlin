784f23d34dc6f335b24e990682a541e0a8ab9b61
Miguel Del Corso <migueldelcorso@gmail.com>
2025-05-24 13:02:30 -0300

    Improved code on Day17.kt, now it reuses the same code for both parts, also removed the useless cost

--- src/Day17.kt
+++ src/Day17.kt
@@ -21,25 +21,32 @@ private fun findEntity(grid: List<List<Entity>>, entity2Search: Entity): Pair<In
     return Pair(-1,-1)
 }
 
-private data class State(val row: Int, val col: Int, val input: String, val cost: Int)
+private data class State(val row: Int, val col: Int, val input: String)
 
-private fun part1(input: String): String {
+private fun part(input: String, isPart2: Boolean = false): String {
     val grid = parseInput()
     val start = findEntity(grid,Entity.START)
     val endPoint = findEntity(grid, Entity.VAULT)
 
     val queue = ArrayDeque<State>()
-    val startState = State(start.first, start.second, input, 0)
+    val startState = State(start.first, start.second, input)
     queue.add(startState)
 
+    var maxLength = -1
+    var minLength = ""
+    val visited = mutableSetOf<String>()
     while(queue.isNotEmpty()) {
-        val (row, col, input, cost) = queue.removeFirst()
+        val (row, col, input) = queue.removeFirst()
+        val stateKey = "$row,$col,$input"
+        if(stateKey in visited) continue
+        visited.add(stateKey)
 
-        if(row == endPoint.first -1 && col == endPoint.second -1) {
+        if(row == endPoint.first - 1 && col == endPoint.second - 1) {
             val result = input.toList().filter { it.isUpperCase() }.joinToString("")
-            return result
+            if(result.length > maxLength) maxLength = result.length
+            if(result.length < minLength.length || minLength == "") minLength = result
+            continue
         }
-
         for (direction in DeltaDirection.entries) {
             val newRow = row + direction.rowDelta
             val newCol = col + direction.colDelta
@@ -48,27 +55,15 @@ private fun part1(input: String): String {
             if(entity == null) continue
             if(entity != Entity.DOOR) continue
 
-
             val (up, down, left, right) = input.md5().take(4).toList().map { listOf('b', 'c','d', 'e', 'f').contains(it) }
-
-            if(up && direction.name == "UP") {
-                queue.add(State(newRow-1, newCol, "${input}U", cost + 1))
-            }
-            if(down && direction.name == "DOWN") {
-                queue.add(State(newRow+1, newCol, "${input}D", cost + 1))
-            }
-            if(left && direction.name == "LEFT") {
-                queue.add(State(newRow, newCol-1, "${input}L", cost + 1))
-            }
-            if(right && direction.name == "RIGHT") {
-                queue.add(State(newRow, newCol+1, "${input}R", cost + 1))
-            }
+            if(up && direction.name == "UP") queue.add(State(newRow-1, newCol, "${input}U"))
+            if(down && direction.name == "DOWN") queue.add(State(newRow+1, newCol, "${input}D"))
+            if(left && direction.name == "LEFT") queue.add(State(newRow, newCol-1, "${input}L"))
+            if(right && direction.name == "RIGHT") queue.add(State(newRow, newCol+1, "${input}R"))
         }
     }
-    return ""
+    return if(isPart2) maxLength.toString() else minLength
 }
-
-
 
 private fun parseInput(): List<List<Entity>> {
     val grid = """
@@ -105,67 +100,12 @@ private fun parseInput(): List<List<Entity>> {
     return newGrid
 }
 
-private fun part2(input: String): Int {
-    val grid = parseInput()
-    val start = findEntity(grid,Entity.START)
-    val endPoint = findEntity(grid, Entity.VAULT)
-
-    val stack = ArrayDeque<State>()
-    val startState = State(start.first, start.second, input, 0)
-    stack.add(startState)
-
-    var maxLength = -1
-    val visited = mutableSetOf<String>()
-
-    while(stack.isNotEmpty()) {
-        val (row, col, input, cost) = stack.removeLast()
-
-
-        val stateKey = "$row,$col,$input"
-
-        if(stateKey in visited) continue
-        visited.add(stateKey)
-
-        if(row == endPoint.first - 1 && col == endPoint.second - 1) {
-            val result = input.toList().filter { it.isUpperCase() }.joinToString("")
-            if(result.length > maxLength) maxLength = result.length
-            continue
-        }
-
-        for (direction in DeltaDirection.entries) {
-            val newRow = row + direction.rowDelta
-            val newCol = col + direction.colDelta
-
-            val entity = grid.getOrNull(newRow)?.getOrNull(newCol)
-            if(entity == null) continue
-            if(entity != Entity.DOOR) continue
-
-
-            val (up, down, left, right) = input.md5().take(4).toList().map { listOf('b', 'c','d', 'e', 'f').contains(it) }
-
-            if(up && direction.name == "UP") {
-                stack.addLast(State(newRow-1, newCol, "${input}U", cost + 1))
-            }
-            if(down && direction.name == "DOWN") {
-                stack.addLast(State(newRow+1, newCol, "${input}D", cost + 1))
-            }
-            if(left && direction.name == "LEFT") {
-                stack.addLast(State(newRow, newCol-1, "${input}L", cost + 1))
-            }
-            if(right && direction.name == "RIGHT") {
-                stack.addLast(State(newRow, newCol+1, "${input}R", cost + 1))
-            }
-        }
-    }
-    return maxLength
-}
-
 fun main() {
     val testInput = readInput("Day17_test").first()
-    check(part1(testInput) == "DRURDRUDDLLDLUURRDULRLDUUDDDRR")
-    check(part2(testInput) == 830)
+    check(part(testInput) == "DRURDRUDDLLDLUURRDULRLDUUDDDRR")
+    check(part(testInput, true).toInt() == 830)
     val input = readInput("Day17").first()
-    check(part1(input) == "DRRDRLDURD")
-    check(part2(input) == 618)
+    check(part(input) == "DRRDRLDURD")
+    check(part(input, true).toInt() == 618)
 }
  
