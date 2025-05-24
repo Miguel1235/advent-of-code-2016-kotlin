7db471296b67628a5a779c18341c17a52d6008cc
Miguel Del Corso <migueldelcorso@gmail.com>
2025-05-24 12:53:58 -0300

    Finished Day17.kt

--- src/Day17.kt
+++ src/Day17.kt
@@ -33,14 +33,10 @@ private fun part1(input: String): String {
     queue.add(startState)
 
     while(queue.isNotEmpty()) {
-//        println("****")
-//        println(queue)
         val (row, col, input, cost) = queue.removeFirst()
-//        println("row: $row, col: $col, cost: $cost input: $input md5: ${input.md5().take(4)}")
 
         if(row == endPoint.first -1 && col == endPoint.second -1) {
             val result = input.toList().filter { it.isUpperCase() }.joinToString("")
-            println("reaching end point!! $result")
             return result
         }
 
@@ -56,33 +52,23 @@ private fun part1(input: String): String {
             val (up, down, left, right) = input.md5().take(4).toList().map { listOf('b', 'c','d', 'e', 'f').contains(it) }
 
             if(up && direction.name == "UP") {
-//                println("We can move up because the door is open")
                 queue.add(State(newRow-1, newCol, "${input}U", cost + 1))
-//                visited[newRow][newCol] = true
-//                continue
             }
             if(down && direction.name == "DOWN") {
-//                println("We can move down because the door is open")
                 queue.add(State(newRow+1, newCol, "${input}D", cost + 1))
-//                visited[newRow][newCol] = true
-//                continue
             }
             if(left && direction.name == "LEFT") {
-//                println("We can move left because the door is open")
                 queue.add(State(newRow, newCol-1, "${input}L", cost + 1))
-//                visited[newRow][newCol] = true
-//                continue
             }
             if(right && direction.name == "RIGHT") {
-//                println("We can move right because the door is open")
                 queue.add(State(newRow, newCol+1, "${input}R", cost + 1))
-//                visited[newRow][newCol] = true
-//                continue
             }
         }
     }
     return ""
 }
+
+
 
 private fun parseInput(): List<List<Entity>> {
     val grid = """
@@ -119,15 +105,67 @@ private fun parseInput(): List<List<Entity>> {
     return newGrid
 }
 
-private fun part2(input: List<String>): Int {
-    return 0
+private fun part2(input: String): Int {
+    val grid = parseInput()
+    val start = findEntity(grid,Entity.START)
+    val endPoint = findEntity(grid, Entity.VAULT)
+
+    val stack = ArrayDeque<State>()
+    val startState = State(start.first, start.second, input, 0)
+    stack.add(startState)
+
+    var maxLength = -1
+    val visited = mutableSetOf<String>()
+
+    while(stack.isNotEmpty()) {
+        val (row, col, input, cost) = stack.removeLast()
+
+
+        val stateKey = "$row,$col,$input"
+
+        if(stateKey in visited) continue
+        visited.add(stateKey)
+
+        if(row == endPoint.first - 1 && col == endPoint.second - 1) {
+            val result = input.toList().filter { it.isUpperCase() }.joinToString("")
+            if(result.length > maxLength) maxLength = result.length
+            continue
+        }
+
+        for (direction in DeltaDirection.entries) {
+            val newRow = row + direction.rowDelta
+            val newCol = col + direction.colDelta
+
+            val entity = grid.getOrNull(newRow)?.getOrNull(newCol)
+            if(entity == null) continue
+            if(entity != Entity.DOOR) continue
+
+
+            val (up, down, left, right) = input.md5().take(4).toList().map { listOf('b', 'c','d', 'e', 'f').contains(it) }
+
+            if(up && direction.name == "UP") {
+                stack.addLast(State(newRow-1, newCol, "${input}U", cost + 1))
+            }
+            if(down && direction.name == "DOWN") {
+                stack.addLast(State(newRow+1, newCol, "${input}D", cost + 1))
+            }
+            if(left && direction.name == "LEFT") {
+                stack.addLast(State(newRow, newCol-1, "${input}L", cost + 1))
+            }
+            if(right && direction.name == "RIGHT") {
+                stack.addLast(State(newRow, newCol+1, "${input}R", cost + 1))
+            }
+        }
+    }
+    return maxLength
 }
 
 fun main() {
     val testInput = readInput("Day17_test").first()
     check(part1(testInput) == "DRURDRUDDLLDLUURRDULRLDUUDDDRR")
-
+    check(part2(testInput) == 830)
     val input = readInput("Day17").first()
     check(part1(input) == "DRRDRLDURD")
+    check(part2(input) == 618)
 }
  
